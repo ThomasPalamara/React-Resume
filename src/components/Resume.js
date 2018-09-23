0497a0441ac8eb29417a3cc13161ae95d5ab8e88
ThomasPalamara <toma06pal@gmail.com>
2018-09-23 22:28:18 +0100

    WIP Projects

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Introduction from "./Introduction";
 import Skills from "./Skills";
+import Projects from "./Projects";
 
 const Resume = (props) => (
     <div>
         <Header />
         <Introduction />
         <Skills />
+        <Projects />
     </div>
 );
 
