e6beaef68e064643aab8ca461b15f0e346799128
ThomasPalamara <toma06pal@gmail.com>
2018-09-23 21:03:05 +0100

    Skills done

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -5,7 +5,7 @@ import { Row, Col } from 'antd';
 const Introduction = (props) => (
     <ResumeContext.Consumer>
         {resume => (
-            <div className="container">
+            <div id="introduction" className="container section">
                 <Row type="flex" justify="center">
                     <Col span={16}>
                         <h3 className="section-title">{resume.introduction.title}</h3>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,31 +1,45 @@
 import React from "react";
 import { ResumeContext } from '../context/ResumeContext';
 import { Row, Col } from 'antd';
+import frontEnd from '../img/icons/front-end.svg';
+import tools from '../img/icons/tools.svg';
+import backEnd from '../img/icons/back-end.svg';
+import languages from '../img/icons/languages.svg';
+import design from '../img/icons/design.svg';
+
+const svgs = [frontEnd, tools, backEnd, languages, design]
 
 const Introduction = (props) => (
     <ResumeContext.Consumer>
         {resume => (
-            <Row type="flex" justify="center" className="primary-bg">
+            <Row type="flex" justify="center" className="section primary-bg">
                 <Col span={16}>
                     <h3 className="section-title">{resume.skills.title}</h3>
                     <div className="divider" />
                     <div className="card-row first">
+                        <svg viewBox="0 0 32 32">
+                            <use href={design}></use>
+                        </svg>
                     {resume.skills.categories.map((category, i) => (
-                                <div className={`card-col ${i<3 ? 'third' : 'half'}`}>
-                                    <div className="card">
-                                        <h4>{category.title}</h4>
-                                        <ul>
-                                        {category.elements.map(element => (
-                                            <li>{element}</li>
-                                        ))}
-                                        </ul>
-                                    </div>
-                                </div>
-                            ))}
+                        <div key={i} className={`card-col ${i < 3 ? 'third' : 'half'}`}>
+                            <div className="card">
+                                <figure className="skills-icon">
+                                    <img src={svgs[i]} alt={`Icon ${category.title}`} />
+                                </figure>
+                                <h4>{category.title}</h4>
+                                <p>{category.description}</p>
+                                <ul>
+                                    {category.elements.map((element, i) => (
+                                        <li key={i}>{element}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    ))}
                     </div>
                 </Col>
             </Row>
-        )}
+    )}
     </ResumeContext.Consumer>
 );
 
