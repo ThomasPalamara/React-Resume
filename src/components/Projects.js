5b64537f0761d09f350dbfaf6fcb29a89afeabc2
ThomasPalamara <toma06pal@gmail.com>
2018-09-24 22:59:13 +0100

    WIP Scroll animation Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,37 +2,67 @@ import React from "react";
 import { ResumeContext } from '../context/ResumeContext';
 import { Row, Col } from 'antd';
 
-const Projects = (props) => (
-    <ResumeContext.Consumer>
-        {resume => (
-            <div id="projects" className="container section">
-                <Row type="flex" justify="center">
-                    <Col span={16}>
-                        <h3 className="section-title">{resume.projects.title}</h3>
-                        <div className="divider"></div>
-                        <div>{resume.projects.text}</div>
-                        <div>
-                            {resume.projects.elements.map(element => (
-                                <div className="project-item">
-                                    <h3>{element.title}</h3>
-                                    <p>{element.description}</p>
 
-                                    <div className="project-tech">
-                                        <p>Used in this project : </p>
-                                        <ul>
-                                            {element.techs.map(tech => (
-                                                <li> {tech}</li>
-                                            ))}
-                                        </ul>
-                                    </div>
+class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll() {
+
+        const scrollTop = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop)
+        var winHeight = window.innerHeight;
+
+        // Annoying to compute doc height due to browser inconsistency
+        var body = document.body;
+        var html = document.documentElement;
+        var docHeight = Math.max( body.scrollHeight, body.offsetHeight, 
+                        html.clientHeight, html.scrollHeight, html.offsetHeight );
+     
+        var value = document.body.scrollTop;
+
+        console.log(scrollTop);
+    }
+    render() {
+        return (
+            <ResumeContext.Consumer>
+                {resume => (
+                    <div id="projects" className="container section">
+                        <Row type="flex" justify="center">
+                            <Col span={16}>
+                                <h3 className="section-title">{resume.projects.title}</h3>
+                                <div className="divider"></div>
+                                <div>{resume.projects.text}</div>
+                                <div>
+                                    {resume.projects.elements.map(element => (
+                                        <div className="project-item">
+                                            <div className="tag">APT</div>
+                                            <h3>{element.title}</h3>
+                                            <p>{element.description}</p>
+
+                                            <div className="project-tech">
+                                                <p>Used in this project : </p>
+                                                <ul>
+                                                    {element.techs.map(tech => (
+                                                        <li> {tech}</li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    ))}
                                 </div>
-                            ))}
-                        </div>
-                    </Col>
-                </Row>
-            </div>
-        )}
-    </ResumeContext.Consumer>
-);
+                            </Col>
+                        </Row>
+                    </div>
+                )}
+            </ResumeContext.Consumer>
+        );
+    }
+}
 
 export default Projects;
